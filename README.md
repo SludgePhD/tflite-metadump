d1773b91e4a953e475c8fa67182a0916a55635f3
Sludge <96552222+SludgePhD@users.noreply.github.com>
2024-07-28 23:03:43 +0200

    bush didn't *do* 9/11 but his politics provoked it

---
+++ README.md
@@ -0,0 +1,6 @@
+CLI tool that extracts and prints [TFLite metadata] from a `.tflite` file.
+
+`metadata_scheme.fbs` was imported from `tflite-support` commit [87f27a9306451d627a196696c2c7babc6e137e82]
+
+[TFLite metadata]: https://www.tensorflow.org/lite/models/convert/metadata
+[87f27a9306451d627a196696c2c7babc6e137e82]: https://github.com/tensorflow/tflite-support/commit/87f27a9306451d627a196696c2c7babc6e137e82
---
+++ src/main.rs
@@ -0,0 +1,193 @@
+use core::fmt;
+use std::{env, fs, ops};
+
+use anyhow::bail;
+use metadata::tflite::{AssociatedFile, ModelMetadata, ProcessUnit, TensorMetadata};
+
+#[allow(warnings)]
+#[path = "../generated/metadata_schema_generated.rs"]
+mod metadata;
+
+#[allow(warnings)]
+#[path = "../generated/schema_v3c_generated.rs"]
+mod v3c;
+
+fn main() -> anyhow::Result<()> {
+    let args = env::args_os().skip(1).collect::<Vec<_>>();
+    let tflite = match &*args {
+        [path] => fs::read(path)?,
+        _ => bail!("usage: tflite-metadump <model.tflite>"),
+    };
+
+    let tflite = flatbuffers::root::<v3c::tflite::Model>(&tflite)?;
+    let Some(metadata) = tflite.metadata() else {
+        bail!("model contains no metadata entries")
+    };
+
+    let Some(metadata) = metadata
+        .iter()
+        .find(|meta| meta.name() == Some("TFLITE_METADATA"))
+    else {
+        bail!("model contains no `TFLITE_METADATA` entry")
+    };
+
+    let Some(buffers) = tflite.buffers() else {
+        bail!("model contains no buffer list")
+    };
+
+    let buffer = buffers.get(metadata.buffer() as usize);
+    let metadata_bytes = buffer.data().map_or(&[][..], |v| v.bytes());
+
+    let meta = flatbuffers::root::<ModelMetadata>(&metadata_bytes)?;
+    if let Some(name) = meta.name() {
+        println!("name: {name}");
+    }
+    if let Some(description) = meta.description() {
+        println!("description: {description}");
+    }
+    if let Some(version) = meta.version() {
+        println!("version: {version}");
+    }
+    if let Some(author) = meta.author() {
+        println!("author: {author}");
+    }
+    if let Some(license) = meta.license() {
+        println!("license: {license}");
+    }
+    if let Some(min_parser_version) = meta.min_parser_version() {
+        println!("min_parser_version: {min_parser_version}");
+    }
+    if let Some(assoc) = meta.associated_files() {
+        println!("{} associated file(s):", assoc.len());
+        print_assoc_files(Indent(0), assoc.iter());
+    }
+    if let Some(subs) = meta.subgraph_metadata() {
+        println!("{} subgraph(s)", subs.len());
+        for sub in subs.iter() {
+            println!("- name: {}", sub.name().unwrap_or("<unnamed>"));
+            if let Some(desc) = sub.description() {
+                println!("  description: {desc}");
+            }
+            if let Some(assoc) = meta.associated_files() {
+                println!("  {} associated file(s):", assoc.len());
+                print_assoc_files(Indent(1), assoc.iter());
+            }
+            if let Some(inp) = sub.input_tensor_metadata() {
+                println!("  - {} input tensor(s) with metadata", inp.len());
+                print_tensor_meta(Indent(2), inp.iter());
+            }
+            if let Some(outp) = sub.output_tensor_metadata() {
+                println!("  - {} output tensor(s) with metadata", outp.len());
+                print_tensor_meta(Indent(2), outp.iter());
+            }
+            if let Some(inp) = sub.input_process_units() {
+                println!("  - {} input tensor process units", inp.len());
+                print_proc(Indent(2), inp.iter());
+            }
+            if let Some(outp) = sub.output_process_units() {
+                println!("  - {} output tensor process units", outp.len());
+                print_proc(Indent(2), outp.iter());
+            }
+            if let Some(groups) = sub.input_tensor_groups() {
+                println!("  - {} input tensor groups", groups.len());
+                for group in groups.iter() {
+                    println!("    - {group:?}");
+                }
+            }
+            if let Some(groups) = sub.output_tensor_groups() {
+                println!("  - {} output tensor groups", groups.len());
+                for group in groups.iter() {
+                    println!("    - {group:?}");
+                }
+            }
+            if let Some(custom) = sub.custom_metadata() {
+                println!("  - {} custom metadata entries", custom.len());
+                for custom in custom.iter() {
+                    println!("    - name: {}", custom.name().unwrap_or("<unnamed>"));
+                    println!("      {} bytes", custom.data().map_or(0, |v| v.len()));
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn print_tensor_meta<'a>(indent: Indent, meta: impl Iterator<Item = TensorMetadata<'a>>) {
+    for tens in meta {
+        println!("{indent}- name: {}", tens.name().unwrap_or("<unnamed>"));
+        if let Some(desc) = tens.description() {
+            println!("{indent}  description: {desc}");
+        }
+        if let Some(dims) = tens.dimension_names() {
+            println!("{indent}  dimension names: {dims:?}");
+        }
+        if let Some(cont) = tens.content() {
+            println!("{indent}  content: {cont:?}");
+        }
+        if let Some(stats) = tens.stats() {
+            println!("{indent}  stats: {stats:?}");
+        }
+        if let Some(assoc) = tens.associated_files() {
+            println!("{indent}  {} associated file(s):", assoc.len());
+            print_assoc_files(indent + 1, assoc.iter());
+        }
+        if let Some(proc) = tens.process_units() {
+            println!("{indent}  {} process unit(s):", proc.len());
+            print_proc(indent + 1, proc.iter());
+        }
+    }
+}
+
+fn print_assoc_files<'a>(indent: Indent, assoc: impl Iterator<Item = AssociatedFile<'a>>) {
+    for file in assoc {
+        println!("{indent}- name: {}", file.name().unwrap_or("<unnamed>"));
+        if let Some(desc) = file.description() {
+            println!("{indent}  description: {desc}");
+        }
+        println!("{indent}  type: {:?}", file.type_());
+        if let Some(locale) = file.locale() {
+            println!("{indent}  locale: {locale}");
+        }
+        if let Some(version) = file.version() {
+            println!("{indent}  version: {version}");
+        }
+    }
+}
+
+fn print_proc<'a>(indent: Indent, proc: impl Iterator<Item = ProcessUnit<'a>>) {
+    for pu in proc {
+        if let Some(options) = pu.options_as_normalization_options() {
+            println!("{indent}- NormalizationOptions: {:?}", options);
+        } else if let Some(options) = pu.options_as_score_calibration_options() {
+            println!("{indent}- ScoreCalibrationOptions: {:?}", options);
+        } else if let Some(options) = pu.options_as_score_thresholding_options() {
+            println!("{indent}- ScoreThresholdingOptions: {:?}", options);
+        } else if let Some(options) = pu.options_as_bert_tokenizer_options() {
+            println!("{indent}- BertTokenizerOptions: {:?}", options);
+        } else if let Some(options) = pu.options_as_sentence_piece_tokenizer_options() {
+            println!("{indent}- SentencePieceTokenizerOptions: {:?}", options);
+        } else if let Some(options) = pu.options_as_regex_tokenizer_options() {
+            println!("{indent}- RegexTokenizerOptions: {:?}", options);
+        } else {
+            println!("{indent}- unknown options type: {:?}", pu.options_type());
+        }
+    }
+}
+
+#[derive(Copy, Clone)]
+struct Indent(usize);
+
+impl ops::Add<usize> for Indent {
+    type Output = Self;
+
+    fn add(self, rhs: usize) -> Self::Output {
+        Self(self.0 + rhs)
+    }
+}
+
+impl fmt::Display for Indent {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str(&"  ".repeat(self.0))
+    }
+}
